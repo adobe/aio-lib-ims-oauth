90f3a0b9224f7cf6370c2a0796bca3e1f2918eb3
Shazron Abdullah (shaz) <shazron@gmail.com>
2019-12-10 11:11:41 +0800

    fix: linting issues, add eslint shared config

--- src/electron.js
+++ src/electron.js
@@ -10,53 +10,53 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-const electronPath = require('electron');
-const { execFile } = require('child_process');
-const debug = require('debug')('@adobe/aio-lib-core-ims-oauth/electron');
+const electronPath = require('electron')
+const { execFile } = require('child_process')
+const debug = require('debug')('@adobe/aio-lib-core-ims-oauth/electron')
 
 class Electron {
-    constructor(appUrl, callbackUrl, force) {
-        this.appUrl = appUrl;
-        this.callbackUrl = callbackUrl;
-        this.force = force;
-    }
+  constructor (appUrl, callbackUrl, force) {
+    this.appUrl = appUrl
+    this.callbackUrl = callbackUrl
+    this.force = force
+  }
 
-    launch(exitCallback) {
-        debug("launch(%o)", exitCallback);
+  launch (exitCallback) {
+    debug('launch(%o)', exitCallback)
 
-        const args = [
+    const args = [
             `${__dirname}/../lib`,
             this.appUrl,
             this.callbackUrl,
             this.force
-        ];
+    ]
 
-        this.childProcess = execFile(electronPath,
-            args,
-            (err, stdout, stderr) => {
-                if (err) {
-                    debug("  > ERR: %s", stderr);
-                    const error = JSON.parse(stderr);
-                    exitCallback(new Error(error.message), error.state);
-                } else {
-                    debug("  > OK: %s", stdout);
-                    const result = JSON.parse(stdout);
-                    exitCallback(result.code, result.state);
-                }
-            }
-        );
-
-        return this;
-    }
-
-    terminate() {
-        if (this.childProcess) {
-            this.childProcess.kill();
-            this.childProcess = undefined;
+    this.childProcess = execFile(electronPath,
+      args,
+      (err, stdout, stderr) => {
+        if (err) {
+          debug('  > ERR: %s', stderr)
+          const error = JSON.parse(stderr)
+          exitCallback(new Error(error.message), error.state)
+        } else {
+          debug('  > OK: %s', stdout)
+          const result = JSON.parse(stdout)
+          exitCallback(result.code, result.state)
         }
+      }
+    )
 
-        return this;
+    return this
+  }
+
+  terminate () {
+    if (this.childProcess) {
+      this.childProcess.kill()
+      this.childProcess = undefined
     }
+
+    return this
+  }
 }
 
-module.exports = Electron;
+module.exports = Electron
--- src/ims-oauth.js
+++ src/ims-oauth.js
@@ -10,39 +10,35 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-const Electron = require('./electron');
-const debug = require('debug')('@adobe/aio-lib-core-ims-oauth/ims-oauth');
+const Electron = require('./electron')
+const debug = require('debug')('@adobe/aio-lib-core-ims-oauth/ims-oauth')
 
-// The ims-login hook for OAuth2 (SUSI) is taking care of calling IMS
-// the function takes the
+function configMissingKeys (configData) {
+  if (!configData) {
+    return false
+  }
 
-function configMissingKeys(configData) {
-    if (!configData) {
-        return false;
+  const missingKeys = []
+  const requiredKeys = ['callback_url', 'client_id', 'client_secret', 'scope']
+
+  requiredKeys.forEach(key => {
+    if (!configData[key]) {
+      missingKeys.push(key)
     }
+  })
 
-    const missingKeys = [];
-    const requiredKeys = ['callback_url', 'client_id', 'client_secret', 'scope'];
-
-    requiredKeys.forEach(key => {
-        if (!configData[key]) {
-            missingKeys.push(key)
-        }
-    })
-
-    return missingKeys
+  return missingKeys
 }
 
 const canSupportSync = (configData) => configMissingKeys(configData).length === 0
 
-async function canSupport(configData) {
-    const missingKeys = configMissingKeys(configData)
-    if (missingKeys.length === 0) {
-          return Promise.resolve(true);
-    } else {
-        // TODO: Indicate that this is not really an error but just a possibility
-        return Promise.reject(`OAuth2 not supported due to some missing properties: ${missingKeys}`);
-    }
+async function canSupport (configData) {
+  const missingKeys = configMissingKeys(configData)
+  if (missingKeys.length === 0) {
+    return Promise.resolve(true)
+  } else {
+    return Promise.reject(new Error(`OAuth2 not supported due to some missing properties: ${missingKeys}`))
+  }
 }
 
 // The result of the browser and proxy interaction, which can take values
@@ -53,7 +49,7 @@ async function canSupport(configData) {
 //         (1) if the user kills the browser before completing
 //         (2) if the access token cannot be generated after the user provided
 //             the credentials
-let webResult = undefined;
+let gWebResult
 
 /**
  * Called when the Electron app driving the SUSI flow terminates.
@@ -66,41 +62,41 @@ let webResult = undefined;
  *          which may be a string in the success case or an Error if the
  *          authorization code is not provided.
  */
-function electronCallback(result, state) {
-    debug("electronCallback(%o, %s)", result, state);
-    webResult = result ? result : new Error("No result received from web app");
-    debug("  > %o", webResult);
+function electronCallback (result, state) {
+  debug('electronCallback(%o, %s)', result, state)
+  gWebResult = result || new Error('No result received from web app')
+  debug('  > %o', gWebResult)
 }
 
-async function setupWeb(ims, config, state, force) {
-    debug("setupWeb(config=%o, state=%s, force=%s)", config, force);
-    const appUrl = ims.getSusiUrl(config.client_id, config.scope, config.callback_url, state);
-    debug("  > appUrl=%s", appUrl);
-    electron = new Electron(appUrl, config.callback_url, force).launch(electronCallback);
+async function setupWeb (ims, config, state, force) {
+  debug('setupWeb(config=%o, state=%s, force=%s)', config, force)
+  const appUrl = ims.getSusiUrl(config.client_id, config.scope, config.callback_url, state)
+  debug('  > appUrl=%s', appUrl)
+  return new Electron(appUrl, config.callback_url, force).launch(electronCallback)
 }
 
-async function checkWebResult() {
-    if (!webResult) {
-        debug("checkWebResult: No result yet, continue polling");
-        return new Promise(resolve => setTimeout(resolve.bind(null), 100)).then(checkWebResult);
-    } else if (webResult instanceof Error) {
-        debug("checkWebResult: Rejecting on error: %o", webResult);
-        return Promise.reject(webResult);
-    } else {
-        debug("checkWebResult: Resolving with code: %s", webResult);
-        return Promise.resolve(webResult);
-    }
+async function checkWebResult () {
+  if (!gWebResult) {
+    debug('checkWebResult: No result yet, continue polling')
+    return new Promise(resolve => setTimeout(resolve.bind(null), 100)).then(checkWebResult)
+  } else if (gWebResult instanceof Error) {
+    debug('checkWebResult: Rejecting on error: %o', gWebResult)
+    return Promise.reject(gWebResult)
+  } else {
+    debug('checkWebResult: Resolving with code: %s', gWebResult)
+    return Promise.resolve(gWebResult)
+  }
 }
 
-async function imsLogin(ims, config, force) {
-    const state = "oauth-imslogin-" + Date.now();
-    return canSupport(config)
-        .then(() => setupWeb(ims, config, state, force))
-        .then(checkWebResult)
-        .then(authorizationCode => ims.getAccessToken(authorizationCode, config.client_id, config.client_secret, config.scope))
+async function imsLogin (ims, config, force) {
+  const state = 'oauth-imslogin-' + Date.now()
+  return canSupport(config)
+    .then(() => setupWeb(ims, config, state, force))
+    .then(checkWebResult)
+    .then(authorizationCode => ims.getAccessToken(authorizationCode, config.client_id, config.client_secret, config.scope))
 }
 
 module.exports = {
-    supports: canSupportSync,
-    imsLogin
+  supports: canSupportSync,
+  imsLogin
 }
