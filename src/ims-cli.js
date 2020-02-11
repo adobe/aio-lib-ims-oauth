f55d4e94af40604b0f0ee3cf8682c9aad0ae823e
Shazron Abdullah <shaz@adobe.com>
2020-02-11 12:27:00 +0800

    feat: add ims-cli oauth plugin (default cli context) (#13)

---
+++ src/ims-cli.js
@@ -0,0 +1,75 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const login = require('./login')
+
+const CLI_BARE_OUTPUT = '$cli.bare-output'
+
+/**
+ * Returns the missing keys (if any) in the configuration data.
+ *
+ * @private
+ * @param {object} configData the configuration data for this plugin
+ * @returns {Array} the missing keys, if any
+ */
+function configMissingKeys (configData) {
+  if (!configData) {
+    return false
+  }
+
+  const missingKeys = []
+  const requiredKeys = [CLI_BARE_OUTPUT]
+
+  requiredKeys.forEach(key => {
+    if (!configData[key]) {
+      missingKeys.push(key)
+    }
+  })
+
+  return missingKeys
+}
+
+const canSupportSync = (configData) => configMissingKeys(configData).length === 0
+
+/**
+ * canSupport resolves to true if the plugin supports the config.
+ *
+ * @param {object} configData the configuration data for this plugin
+ * @returns {Promise} resolves to true if the plugin can support the config
+ */
+async function canSupport (configData) {
+  const missingKeys = configMissingKeys(configData)
+  if (missingKeys.length === 0) {
+    return Promise.resolve(true)
+  } else {
+    return Promise.reject(new Error(`OAuth2 for cli not supported due to some missing properties: ${missingKeys}`))
+  }
+}
+
+/**
+ * Log in function for this plugin.
+ *
+ * @param {object} ims the Ims object
+ * @param {object} config the configuration data for this plugin
+ * @returns {Promise} resolves with the token data
+ */
+async function imsLogin (ims, config) {
+  return canSupport(config)
+    .then(() => {
+      const options = { bare: config[CLI_BARE_OUTPUT] }
+      return login(options)
+    })
+}
+
+module.exports = {
+  supports: canSupportSync,
+  imsLogin
+}
---
+++ test/ims-cli.test.js
@@ -0,0 +1,56 @@
+/*
+Copyright 2018 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const plugin = require('../src/ims-cli')
+const login = require('../src/login')
+
+jest.mock('../src/login')
+
+const gIms = {
+  getAccessToken: jest.fn()
+}
+
+const gConfig = {
+  '$cli.bare-output': true
+}
+
+beforeEach(() => {
+  jest.restoreAllMocks()
+})
+
+test('has ims plugin interface', () => {
+  expect(typeof plugin.supports).toEqual('function')
+  expect(typeof plugin.imsLogin).toEqual('function')
+})
+
+test('supports() interface', () => {
+  expect(plugin.supports(gConfig)).toBeTruthy()
+  expect(plugin.supports({})).toBeFalsy()
+  expect(plugin.supports()).toBeFalsy()
+})
+
+test('imsLogin() interface', async () => {
+  const myAccessToken = 'my-access-token'
+
+  login.mockImplementation((config) => {
+    return myAccessToken
+  })
+
+  // normal acceptable config
+  await expect(plugin.imsLogin(gIms, gConfig)).resolves.toEqual(myAccessToken)
+
+  // config missing a property
+  const configMissingProperties = Object.assign({}, gConfig)
+  delete configMissingProperties['$cli.bare-output']
+  await expect(plugin.imsLogin(gIms, configMissingProperties)).rejects.toEqual(
+    new Error('OAuth2 for cli not supported due to some missing properties: $cli.bare-output'))
+})
