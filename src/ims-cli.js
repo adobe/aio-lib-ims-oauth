fafba1b80d8fea660d994a319217147297f360ff
Justin Edelson <justin@justinedelson.com>
2022-01-06 07:57:33 -0500

    feat: support specifying browser for URL opening. fixes #43

--- src/ims-cli.js
+++ src/ims-cli.js
@@ -73,7 +73,7 @@ async function imsLogin (ims, config) {
   aioLogger.debug(`imsLogin config: ${JSON.stringify(config)}`)
   return canSupport(config)
     .then(() => {
-      const options = { bare: config[CLI_BARE_OUTPUT], env: config.env, timeout: config.timeout, open: config.open }
+      const options = { bare: config[CLI_BARE_OUTPUT], env: config.env, timeout: config.timeout, open: config.open, browser: config.browser }
       return login(options)
     })
 }
